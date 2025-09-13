ac9dc5d228c58f537c0c53fb524653f580a14fea
Nayandeep1431 <jprashu19052001@gmail.com>
2025-09-14 04:17:21 +0530

    fixed conflict

--- files.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/upload-receipt', (req, res) => {
-  res.json({ success: true, message: 'File upload coming soon' });
-});
-
-module.exports = router;
